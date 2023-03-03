bafdee6471058d51fb14d70a239a36da47255b3b
Martin Apiwat <martin.apiwat@gmail.com>
2023-03-03 12:06:20 +0100

    added dynamic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import Channel from "./views/Channel";
 
 export default function App() {
   return (
     <div className="main-container">
       <Sidebar />
-      <div className="chat-section">chat-section</div>
+      <Routes>
+        <Route
+          path="/"
+          element={<div className="chat-selection">chat selection</div>}
+        />
+        <Route path="/:id" element={<Channel />} />
+      </Routes>
     </div>
   );
 }
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { Hash } from "react-feather";
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
-  const [channels, setChannels] = useState(null);
+  const { data: channels } = useQuery({
+    queryKey: ["channels"],
+    queryFn: () =>
+      fetch("http://localhost:3000/channels").then((data) => data.json()),
+  });
 
-  useEffect(() => {
-    async function getChannels() {
-      const response = await fetch("http://localhost:3000/channels");
-      const data = await response.json();
-      console.log(data);
-      setChannels(data);
-    }
-    getChannels();
-  }, []);
   return (
     <div className="sidebar">
       <h1 className="sidebar-header">Martins Slack</h1>
       {channels
         ? channels.map((channel) => (
-            <div className="sidebar-row">
+            <Link
+              to={`/${channel._id}`}
+              className="sidebar-row"
+              key={channel._id}
+            >
               <Hash />
               {channel.name}
-            </div>
+            </Link>
           ))
         : "loading..."}
     </div>
